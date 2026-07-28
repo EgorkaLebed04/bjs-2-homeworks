0af41c33b4c6dd9e28bec9e9361dfc6d13545aef
EgorkaLebed04 <egorka04050806@gmail.com>
2026-07-28 17:04:49 +0700

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,11 +3,11 @@ function solveEquation(a, b, c) {
   let arr = [];
   let num;
   let num2;
-  let D = b**-4*a*c;
-  if (!D) {
+  let d = b**-4*a*c;
+  if (!d) {
     num = -b/(2*a);
     arr.push(num);
-  } else if (D > 0) {
+  } else if (d > 0) {
     num = (-b + Math.sqrt(d))/(2*a);
     num2 = (-b - Math.sqrt(d))/(2*a);
     arr.push(num, num2)
