80f7350320e6b3bc197bcd004030e389e211ffb5
Egorka Lebed <egorka_lebed2001@mail.ru>
2026-08-09 13:48:59 +0700

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,62 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  if (arr.length === 0) {
+    return 0;
+  }
+  let min = arr[0];
+  let max = arr[0];
+  let avg = 0;
+  for (const num of arr) {
+    if (num >= max) {
+      max = num;
+    }
+    if (num <= min) {
+      min = num
+    }
+    // avg += num;
+  }
+  avg = arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
+  return { min: min, max: max, avg: +avg.toFixed(2) };
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  return(+arr.reduce((acc, curr) => acc + curr, 0))
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  return (+(Math.max(...arr) - Math.min(...arr)))
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEven = arr.filter(n => n % 2 === 0).reduce((acc, curr) => acc + curr, 0);
+  let summOdd = arr.filter(n => n % 2 !== 0).reduce((acc, curr) => acc + curr, 0);
 
+  return sumEven - summOdd
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let even = arr.filter(n => n % 2 === 0);
+  return even.reduce((acc, curr) => acc + curr, 0) / even.length
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let result = func(...arrOfArr[i])
+    if (result > maxWorkerResult) {
+      maxWorkerResult = result;
+    }
+  }
+  return maxWorkerResult
 }
