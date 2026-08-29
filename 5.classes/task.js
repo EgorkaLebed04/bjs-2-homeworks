b61b2523804963ba638a0b518dc37a265d33107e
Egorka Lebed <egorka_lebed2001@mail.ru>
2026-08-29 13:08:13 +0700

    update

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -93,3 +93,35 @@ class Library {
         return this.books.splice(index, 1)[0];
     }
 }
+
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+ new DetectiveBook(
+   "Артур Конан Дойл",
+   "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+   2019,
+   1008
+ )
+);
+library.addBook(
+ new FantasticBook(
+   "Аркадий и Борис Стругацкие",
+   "Пикник на обочине",
+   1972,
+   168
+ )
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1919, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1919).name); //"Мурзилка"
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+const editBook = library.giveBookByName("Машина времени");
+editBook.state = 80;
+editBook.fix();
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+library.addBook(editBook)
